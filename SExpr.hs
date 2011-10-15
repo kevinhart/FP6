1ffca380a18d9eb4d20dbccab4dcdfa089bf83e4
Matt MacEwan(GLaDOS) <mrm4677@rit.edu>
2011-10-14 20:57:41 -0400

    SExpr is now Read
    Also it derives Eq.

--- SExpr.hs
+++ SExpr.hs
@@ -1,6 +1,6 @@
 module SExpr where
 
-data SExpr a = Name a | Proc a (SExpr a) | Call (SExpr a) (SExpr a)
+data SExpr a = Name a | Proc a (SExpr a) | Call (SExpr a) (SExpr a) deriving Eq
 
 showsSExpr :: (Show a) => SExpr a -> String -> String
 showsSExpr (Name a) = shows a
@@ -9,3 +9,22 @@ showsSExpr (Call sea seb) = ('(':) . showsSExpr sea . (' ':) . showsSExpr seb . 
 
 instance (Show a) => Show (SExpr a) where
   showsPrec _ x = showsSExpr x
+
+readsSExpr :: (Read a) => ReadS (SExpr a)
+readsSExpr s = [(Proc var expr, z) | ("(",      t) <- lex s,
+                                     ("lambda", u) <- lex t,
+				     ("(",      v) <- lex u,
+                                     (var,      w) <- reads v,
+				     (")",      x) <- lex w,
+                                     (expr,     y) <- readsSExpr x,
+                                     (")",      z) <- lex y         ]
+               ++
+               [(Call exp1 exp2, w) | ("(",  t) <- lex s,
+                                      (exp1, u) <- readsSExpr t,
+                                      (exp2, v) <- readsSExpr u,
+                                      (")",  w) <- lex v         ]
+               ++
+               [(Name nym, t) | (nym, t) <- reads s]
+
+instance (Read a) => Read (SExpr a) where
+  readsPrec _ x = readsSExpr x
