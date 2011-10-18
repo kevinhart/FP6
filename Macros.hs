271692d4fd92f7c5f39023c025f4e22f6b1f404e
Kevin Hart <kevin@k-hart.com>
2011-10-18 13:21:12 -0400

    Added Show and Read for Macro.
    Relatively untested unfortunately, might need to get re-worked slightly.

--- Macros.hs
+++ Macros.hs
@@ -2,20 +2,35 @@ module Macros where
 
 import SExpr
 
-type Macro a = (a,a)
+data Macro a = Macro { macroKey :: String, macroValue :: SExpr a }
 type Macros a = [Macro a]
 
 substitute :: (Eq a, Show a) => Macros a -> SExpr a -> SExpr a
 substitute [] exp = exp
 substitute (x:xs) exp = substitute xs (sub x exp)
-    where
-        sub mac (Name a) = if a == (fst mac) then (Name (snd mac)) else (Name a)
-        sub mac (Proc a se) = if a == (fst mac) then (Proc (snd mac) se) else (Proc a (sub  mac se))
-        sub mac (Call sea seb) = (Call (sub mac sea) (sub mac seb))
+  where
+    sub mac (Name a) = if True then (macroValue mac) else (Name a)
+    sub mac (Proc a se) = Proc a (sub  mac se)
+    sub mac (Call sea seb) = Call (sub mac sea) (sub mac seb)
 
+showsMacro :: (Show a) => Macro a -> String -> String
+showsMacro x = ("(define "++) . ((macroKey x)++) . (" "++) . showsSExpr (macroValue x) . (')':)
+--showsSExpr (Call sea seb) = ('(':) . showsSExpr sea . (' ':) . showsSExpr seb . (')':)
 
-{-
-e1 = [ " (((lambda (x) (lambda (x) x)) a) b) ",
+instance (Show a) => Show (Macro a) where
+  showsPrec _ x = showsMacro x
+
+readsMacro :: (Read a) => ReadS (Macro a)
+readsMacro m = [(Macro key val, x) | ("(",      t) <- lex m,
+                                     ("define", u) <- lex t,
+                                     (key,      v) <- lex u,
+                                     (val,      w) <- readsSExpr v,
+                                     (")",      x) <- lex w]
+
+instance (Read a) => Read (Macro a) where
+  readsPrec _ x = readsMacro x
+
+e1 = [ " (((lambda (\"x\") (lambda (\"x\") \"x\")) \"a\") \"b\") ",
        " (((lambda (x) (lambda (y) x)) y) foo) " ]
        
 m2 = [ " (define true  (lambda (x) (lambda (y) x))) ",
@@ -24,4 +39,3 @@ m2 = [ " (define true  (lambda (x) (lambda (y) x))) ",
 
 e2 = [ " (((if true) this) that) " ,
        " (((if false) this) that) " ]
--}
