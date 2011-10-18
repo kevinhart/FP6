a19c1c955d06593cca2c7958b660b6c490cc9a01
Matt MacEwan(GLaDOS) <mrm4677@rit.edu>
2011-10-18 14:21:24 -0400

    Generalized Macro to have keys of any type
    Fixed 'substitute all the things' problem

--- Macros.hs
+++ Macros.hs
@@ -2,19 +2,19 @@ module Macros where
 
 import SExpr
 
-data Macro a = Macro { macroKey :: String, macroValue :: SExpr a }
+data Macro a = Macro { macroKey :: a, macroValue :: SExpr a }
 type Macros a = [Macro a]
 
 substitute :: (Eq a, Show a) => Macros a -> SExpr a -> SExpr a
 substitute [] exp = exp
 substitute (x:xs) exp = substitute xs (sub x exp)
   where
-    sub mac (Name a) = if True then (macroValue mac) else (Name a)
+    sub mac (Name a) = if a == (macroKey mac) then (macroValue mac) else (Name a)
     sub mac (Proc a se) = Proc a (sub  mac se)
     sub mac (Call sea seb) = Call (sub mac sea) (sub mac seb)
 
 showsMacro :: (Show a) => Macro a -> String -> String
-showsMacro x = ("(define "++) . ((macroKey x)++) . (" "++) . showsSExpr (macroValue x) . (')':)
+showsMacro x = ("(define "++) . (shows (macroKey x)) . (" "++) . showsSExpr (macroValue x) . (')':)
 --showsSExpr (Call sea seb) = ('(':) . showsSExpr sea . (' ':) . showsSExpr seb . (')':)
 
 instance (Show a) => Show (Macro a) where
@@ -23,7 +23,7 @@ instance (Show a) => Show (Macro a) where
 readsMacro :: (Read a) => ReadS (Macro a)
 readsMacro m = [(Macro key val, x) | ("(",      t) <- lex m,
                                      ("define", u) <- lex t,
-                                     (key,      v) <- lex u,
+                                     (key,      v) <- reads u,
                                      (val,      w) <- readsSExpr v,
                                      (")",      x) <- lex w]
 
@@ -33,9 +33,9 @@ instance (Read a) => Read (Macro a) where
 e1 = [ " (((lambda (\"x\") (lambda (\"x\") \"x\")) \"a\") \"b\") ",
        " (((lambda (x) (lambda (y) x)) y) foo) " ]
        
-m2 = [ " (define true  (lambda (\"x\") (lambda (\"y\") \"x\"))) ",
-       " (define false (lambda (\"x\") (lambda (\"y\") \"y\"))) ",
-       " (define if    (lambda (\"cond\") (lambda (\"then\") (lambda (\"else\") ((\"cond\" \"then\") \"else\"))))) " ]
+m2 = [ " (define \"true\"  (lambda (\"x\") (lambda (\"y\") \"x\"))) ",
+       " (define \"false\" (lambda (\"x\") (lambda (\"y\") \"y\"))) ",
+       " (define \"if\"    (lambda (\"cond\") (lambda (\"then\") (lambda (\"else\") ((\"cond\" \"then\") \"else\"))))) " ]
 
 e2 = [ " (((if true) this) that) " ,
        " (((if false) this) that) " ]
