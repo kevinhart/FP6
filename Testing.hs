3406b7b49cc21a7fe9231897cb82f25a35dcf9d9
Kevin Hart <kevin@k-hart.com>
2011-10-18 20:13:16 -0400

    Added remaining exs. from FP2 (esp. recursion)
    Added (ugly-looking) method to prove the y-combinator reduces successfully.

--- Testing.hs
+++ Testing.hs
@@ -39,11 +39,53 @@ countm = [ "(define \"succ\" (lambda (\"n\") (lambda (\"f\") (lambda (\"x\") (\"
            "(define \"pred\" (lambda (\"n\") (((\"n\" (lambda (\"p\") (lambda (\"z\") ((\"z\" (\"succ\" (\"p\" \"true\"))) (\"p\" \"true\"))))) (lambda (\"z\") ((\"z\" \"0\") \"0\"))) \"false\")))" ]
 
 counttest = [ "((\"repeat\" (\"succ\" \"2\")) \"hello\")",
-              "((\"repeat\" (\"pred\" \"3\")) \"hello\")" ]
+              "((\"repeat\" (\"pred\" \"2\")) \"hello\")" ]
               
-              
+-- 6. arithmetic
+
+arithm = [ "(define \"sum\" (lambda (\"m\") (lambda (\"n\") (lambda (\"f\") (lambda (\"x\") ((\"m\" \"f\") ((\"n\" \"f\") \"x\")))))))",
+           "(define \"product\" (lambda (\"m\") (lambda (\"n\") (lambda (\"f\") (\"m\" (\"n\" \"f\"))))))" ]
+
+arithtest = [ "((\"repeat\" ((\"sum\" \"2\") \"3\")) \"hello\")",
+              "((\"repeat\" ((\"product\" \"2\") \"3\")) \"hello\")" ]              
+
+-- 7. predicate
+
+predm = [ "(define \"isZero\" (lambda (\"n\") ((\"n\" (lambda (\"x\") \"false\")) \"true\")))" ]
+
+predtest = [ "(((\"if\" (\"isZero\" \"0\")) \"this\") \"that\")",
+             "(((\"if\" (\"isZero\" \"1\")) \"this\") \"that\")" ]
+
+-- 8. recursion
+
+-- original definitions; NOTE these themselves contain macros
+recurm = [ "(define \"r\" (lambda (\"f\") (lambda (\"n\") (((\"if\" (\"isZero\" \"n\")) \"1\") ((\"product\" \"n\") (\"f\" (\"pred\" \"n\")))))))",
+               "(define \"Y\" (lambda (\"y\") ((lambda (\"x\") (\"y\" (\"x\" \"x\"))) (lambda (\"x\") (\"y\" (\"x\" \"x\"))))))",
+               "(define \"factorial\" (\"Y\" \"r\"))" ]
+               
+recurtest = [ "((\"repeat\" (\"factorial\" ((\"product\" \"2\") \"2\"))) \"hello\")" ]
+
+
 -- combined macros
-macros = (map read (boolsm ++ cnumsm ++ repeatm ++ countm)) :: Macros String
+macros = (map read (boolsm ++ cnumsm ++ repeatm ++ countm ++ arithm ++ predm ++ recurm)) :: Macros String
+
+-- testresult reads a list of strings that are expressions, applies macro substitution,
+-- then applies beta-reduction to produce a list of result SExpr String
+testresult :: [String] -> [SExpr String]
+testresult x = map (beta . substitute macros . read) x
 
 -- example of how to get results of tests
-countresult = map (beta . substitute macros . read) counttest :: [SExpr String]
+countresult = testresult counttest
+arithresult = testresult arithtest
+predresult = testresult predtest
+recurresult = testresult recurtest
+
+-- since many macros contain macros themselves and we want to prove the y-combinator
+-- successfully reduces, (1) apply macro substitutions until no changes are
+-- observed, then (2) apply beta reduction; if a change occurs go to (1) else stop
+fullyReduce result = if reduction == result then result else fullyReduce reduction
+  where
+    reduction = beta $ applyAllMacros result
+    applyAllMacros r = if r == substituted then substituted else applyAllMacros substituted
+      where
+        substituted = substitute macros r
